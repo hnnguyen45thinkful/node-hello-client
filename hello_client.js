0c1acbe4908e399ee56a4bed953ea3e191085385
Hieu Nguyen <hnnguyen45@gmail.com>
2016-11-09 23:29:51 -0800

    Test server with hello-server completed exercise

--- hello_client.js
+++ hello_client.js
@@ -1,7 +1,16 @@
+//var unirest = require('unirest');
+
+//unirest.get('http://www.thinkful.com/').end(function(response) {
+//  console.log('Status:', response.statusCode);
+//  console.log('Headers: ', response.headers);
+//  console.log('Body:', response.body);
+//});
+
+//Exercise below 
 var unirest = require('unirest');
 
-unirest.get('http://www.thinkful.com/').end(function(response) {
-  console.log('Status:', response.statusCode);
-  console.log('Headers: ', response.headers);
-  console.log('Body:', response.body);
+unirest.get('http://localhost:8080/').end(function(response) {
+    console.log('Status:', response.statusCode);
+    console.log('Headers: ', response.headers);
+    console.log('Body:', response.body);
 });
